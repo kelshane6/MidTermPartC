6bfa381134dadec28af8c17e57587f99c269e7e1
kelshane6 <kelshane@sheridancollege.ca>
2023-02-21 11:19:40 -0500

    Created class with my name

---
+++ src/midtermexam_versiona_extensioncode/Shane.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author Shane Kelly
+ */
+public class Shane {
+    
+    // Added branch 
+    
+}
